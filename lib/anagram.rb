d62ccb2d4b1910dea55973dcb57d26fd493bb63f
Muriithi Ian <imuriithiian@gmail.com>
2022-10-27 17:02:55 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize word
+        @word = word        
+    end
+
+    def match words
+        words.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
